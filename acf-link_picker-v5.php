fd8d599a45230120c5cb9c0b64f979ae626c683d
khromov <stanislav.khromov@gmail.com>
2015-01-07 02:09:46 +0100

    Fixed missing textdomain, introduced textdomain constant

--- acf-link_picker-v5.php
+++ acf-link_picker-v5.php
@@ -29,7 +29,7 @@ class acf_field_link_picker extends acf_field {
 		*  label (string) Multiple words, can include spaces, visible when selecting a field type
 		*/
 		
-		$this->label = __('Link Picker', 'acf-link_picker');
+		$this->label = __('Link Picker', ACFLP_TD);
 		
 		
 		/*
@@ -54,10 +54,10 @@ class acf_field_link_picker extends acf_field {
 		*/
 		
 		$this->l10n = array(
-			'yes'		=> __('Yes'),
-			'no'		=> __('No'),
-			'edit_link'	=> __('Edit Link', 'acf-link_picker'),
-			'insert_link'	=> __('Insert Link', 'acf-link_picker')
+			'yes'		=> __('Yes', ACFLP_TD),
+			'no'		=> __('No', ACFLP_TD),
+			'edit_link'	=> __('Edit Link', ACFLP_TD),
+			'insert_link'	=> __('Insert Link', ACFLP_TD)
 		);
 		
 				
@@ -140,24 +140,24 @@ class acf_field_link_picker extends acf_field {
 		?>
 		<div id="link-picker-<?php echo $field['id']; ?>-wrap">
             <p>
-                <?php _e('Currently selected page:', 'acf-link_picker'); ?>
+                <?php _e('Currently selected page:', ACFLP_TD); ?>
                 
                 <input type="hidden" name="<?php echo $field['name']; ?>[url]" id="link-picker-<?php echo $field['id']; ?>-url" value="<?php echo $field['value']['url']; ?>">
                 <input type="hidden" name="<?php echo $field['name']; ?>[title]" id="link-picker-<?php echo $field['id']; ?>-title" value="<?php echo $field['value']['title']; ?>">
                 <input type="hidden" name="<?php echo $field['name']; ?>[target]" id="link-picker-<?php echo $field['id']; ?>-target" value="<?php echo $field['value']['target']; ?>">
                 
                 <div id="link-picker-<?php echo $field['id']; ?>-exists"<?php if (!$exists) { echo ' style="display:none;"'; } ?>>
-                    <?php _e('URL'); ?>: <em id="link-picker-<?php echo $field['id']; ?>-url-label"><a href="<?php echo $field['value']['url']; ?>" target="_blank"><?php echo $field['value']['url']; ?></a></em><br>
-                    <?php _e('Title'); ?>: <em id="link-picker-<?php echo $field['id']; ?>-title-label"><?php echo $field['value']['title']; ?></em><br>
-                    <?php _e('Open link in a new window/tab'); ?>: <em id="link-picker-<?php echo $field['id']; ?>-target-label"><?php if ($field['value']['target'] == '_blank') { _e('Yesssss'); } else { _e('No'); } ?></em>
+                    <?php _e('URL', ACFLP_TD); ?>: <em id="link-picker-<?php echo $field['id']; ?>-url-label"><a href="<?php echo $field['value']['url']; ?>" target="_blank"><?php echo $field['value']['url']; ?></a></em><br>
+                    <?php _e('Title', ACFLP_TD); ?>: <em id="link-picker-<?php echo $field['id']; ?>-title-label"><?php echo $field['value']['title']; ?></em><br>
+                    <?php _e('Open link in a new window/tab', ACFLP_TD); ?>: <em id="link-picker-<?php echo $field['id']; ?>-target-label"><?php if ($field['value']['target'] == '_blank') { _e('Yes', ACFLP_TD); } else { _e('No', ACFLP_TD); } ?></em>
                 </div>
                 <div id="link-picker-<?php echo $field['id']; ?>-none"<?php if ($exists) { echo ' style="display:none;"'; } ?>>
-                    <em><?php _e('No link selected yet', 'acf-link_picker'); ?></em>
+                    <em><?php _e('No link selected yet', ACFLP_TD); ?></em>
                 </div>
             </p>
             <p>
-                <a href="" class="link-btn acf-button grey" id="link-picker-<?php echo $field['id']; ?>"><?php if (!$exists) { _e('Insert Link', 'acf-link_picker'); }else{ _e('Edit Link', 'acf-link_picker'); } ?></a>
-                <a href="" class="link-remove-btn acf-button grey" id="link-picker-<?php echo $field['id']; ?>-remove"<?php if (!$exists) { echo ' style="display:none;"'; } ?>><?php _e('Remove Link', 'acf-link_picker'); ?></a>
+                <a href="" class="link-btn acf-button grey" id="link-picker-<?php echo $field['id']; ?>"><?php if (!$exists) { _e('Insert Link', ACFLP_TD); }else{ _e('Edit Link', ACFLP_TD); } ?></a>
+                <a href="" class="link-remove-btn acf-button grey" id="link-picker-<?php echo $field['id']; ?>-remove"<?php if (!$exists) { echo ' style="display:none;"'; } ?>><?php _e('Remove Link', ACFLP_TD); ?></a>
             </p>
         </div>
         <?php
--- acf-link_picker.php
+++ acf-link_picker.php
@@ -16,7 +16,8 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 // 1. set text domain
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-link_picker', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
+define('ACFLP_TD', 'acf-link_picker');
+load_plugin_textdomain( ACFLP_TD, false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
 
@@ -24,7 +25,7 @@ load_plugin_textdomain( 'acf-link_picker', false, dirname( plugin_basename(__FIL
 // 2. Include field type for ACF5
 // $version = 5 and can be ignored until ACF6 exists
 function include_field_types_link_picker( $version ) {
-	
+
 	include_once('acf-link_picker-v5.php');
 	
 }
@@ -36,7 +37,6 @@ add_action('acf/include_field_types', 'include_field_types_link_picker');
 
 // 3. Include field type for ACF4
 function register_fields_link_picker() {
-	
 	include_once('acf-link_picker-v4.php');
 	
 }
